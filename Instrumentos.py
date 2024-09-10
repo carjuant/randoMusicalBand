2ed55bd8cac64fbc2362fa36ff15c1b36359139d
ElSnatix <santiagoguarguati25@gmail.com>
2024-09-09 22:35:04 -0500

    Se carga el archivo instrumento que tiene las respectivas clases con lo que hace cada instrumento y ademas el archivo musico que tiene los respectivos datos del musico

--- Instrumentos.py
+++ Instrumentos.py
@@ -10,18 +10,25 @@ class Instrumento(ABC):
 class Guitarra(Instrumento):
     def sonar(self):
         print("Sonido de Guitarra")
-    
+    def __str__(self):
+        return "Instrumento: Guitarra"
     
 class Acordeon(Instrumento):
     def sonar(self):
         print ("Sonido de Acordeon")
+    def __str__(self):
+        return "Instrumento: Acordeon"
 
 class Trompeta(Instrumento):
     def sonar(self):
         print("Sonido de Trompeta")
+    def __str__(self):
+        return "Instrumento: Trompeta"
 
 class Maracas (Instrumento):
     def sonar(self):
         print ("Sonido de Maracas")
+    def __str__(self):
+        return "Instrumento: Maracas"
 
 misInstrumentos=[Guitarra(),Acordeon(),Trompeta(),Maracas()]
